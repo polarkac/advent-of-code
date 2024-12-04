5b2ea0214b153cf9dac30f401b7363106b3e982c
Lukas Pohlreich <lukas@pohlreichlukas.eu>
2024-12-04 10:40:22 +0100

    2024-03 Better parsing of input data

--- src/year24/day03.rs
+++ src/year24/day03.rs
@@ -5,114 +5,159 @@ pub fn input() -> &'static str {
 }
 
 pub fn part1(input: &str) -> String {
-    let mut parser = Parser::new();
-    let mut nums = Vec::new();
-    for c in input.chars() {
-        parser.transition(c);
-        if let ParsingState::Final = parser.state {
-            nums.push(parser.multiply());
+    let lexer = lexer(input);
+    let mut total = 0;
+    for s in lexer {
+        if let Symbol::Mul(num1, num2) = s {
+            total += num1.saturating_mul(num2);
         }
     }
 
-    nums.iter().sum::<i32>().to_string()
+    total.to_string()
 }
 
 pub fn part2(input: &str) -> String {
-    String::new()
+    let lexer = lexer(input);
+    let mut total = 0;
+    let mut should_do = true;
+    for s in lexer {
+        match s {
+            Symbol::Do => should_do = true,
+            Symbol::Dont => should_do = false,
+            Symbol::Mul(num1, num2) => {
+                if should_do {
+                    total += num1.saturating_mul(num2);
+                }
+            }
+        }
+    }
+
+    total.to_string()
+}
+
+fn handle_mul(scanner: &mut Scanner) -> Option<Symbol> {
+    scanner.advance_by(4);
+    let mut num1: Vec<i64> = Vec::new();
+    let mut num2: Vec<i64> = Vec::new();
+    let mut add_to_one = true;
+    while !scanner.at_end() {
+        let c = scanner.pop().unwrap();
+        if c == String::from(",") {
+            add_to_one = false;
+            continue;
+        }
+        if c == String::from(")") {
+            let num1 =
+                num1.iter().rev().enumerate().fold(0, |mut num, (i, n)| {
+                    num += n * 10_i64.pow(i as u32);
+
+                    num
+                });
+            let num2 =
+                num2.iter().rev().enumerate().fold(0, |mut num, (i, n)| {
+                    num += n * 10_i64.pow(i as u32);
+
+                    num
+                });
+
+            return Some(Symbol::Mul(num1, num2));
+        }
+        match c.parse() {
+            Ok(num) => {
+                if add_to_one {
+                    num1.push(num);
+                } else {
+                    num2.push(num);
+                }
+            }
+            Err(_) => return None,
+        };
+    }
+    None
+}
+
+fn lexer(input: &str) -> Vec<Symbol> {
+    let mut scanner = Scanner::new(input);
+    let mut lexer = Vec::new();
+
+    while !scanner.at_end() {
+        if let Some("mul(") = scanner.peek(4).as_deref() {
+            if let Some(mul) = handle_mul(&mut scanner) {
+                lexer.push(mul);
+            }
+            continue;
+        }
+        if let Some("do()") = scanner.peek(4).as_deref() {
+            lexer.push(Symbol::Do);
+            scanner.advance_by(4);
+            continue;
+        }
+        if let Some("don't()") = scanner.peek(7).as_deref() {
+            lexer.push(Symbol::Dont);
+            scanner.advance_by(7);
+            continue;
+        }
+        scanner.advance_by(1);
+    }
+
+    lexer
 }
 
 #[derive(Debug)]
-enum ParsingState {
-    Instruction,
-    FirstNum,
-    SecondNum,
-    Final,
+enum Symbol {
+    Mul(i64, i64),
+    Do,
+    Dont,
 }
 
-#[derive(Debug)]
-struct Parser {
-    command: String,
-    num1: String,
-    num2: String,
-    state: ParsingState,
+struct Scanner<'a> {
+    cursor: usize,
+    chars: &'a str,
 }
 
-impl Parser {
-    const ALLOWED_CHARS: [char; 16] = [
-        'm', 'u', 'l', '(', ')', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-        '0', ',',
-    ];
-    pub fn new() -> Self {
-        Self {
-            command: String::new(),
-            num1: String::new(),
-            num2: String::new(),
-            state: ParsingState::Instruction,
-        }
+impl<'a> Scanner<'a> {
+    pub fn new(chars: &'a str) -> Self {
+        Self { cursor: 0, chars }
     }
 
-    pub fn transition(&mut self, c: char) {
-        if !Self::ALLOWED_CHARS.contains(&c) {
-            self.reset();
-            return;
-        }
-        let next_state = self.next_state(c);
-
-        if let None = next_state {
-            match self.state {
-                ParsingState::Instruction => {
-                    self.command.push(c);
-                    if self.command.len() == 4 {
-                        self.command.remove(0);
-                    }
-                },
-                ParsingState::FirstNum => self.num1.push(c),
-                ParsingState::SecondNum => self.num2.push(c),
-                _ => {}
-            }
-        } else if let Some(ParsingState::FirstNum) = next_state {
-            self.state = next_state.unwrap();
-            if self.command != "mul" {
-                self.reset();
-            }
-        } else {
-            self.state = next_state.unwrap();
-        }
+    pub fn peek(&self, len: usize) -> Option<String> {
+        self.chars
+            .get(self.cursor..(self.cursor + len))
+            .map(|v| v.to_string())
     }
 
-    pub fn multiply(&mut self) -> i32 {
-        let num1: i32 = self.num1.parse().unwrap();
-        let num2: i32 = self.num2.parse().unwrap();
-        self.reset();
-
-        num1.saturating_mul(num2)
+    pub fn advance_by(&mut self, by: usize) {
+        self.cursor += by;
     }
 
-    pub fn reset(&mut self) {
-        self.state = ParsingState::Instruction;
-        self.command.clear();
-        self.num1.clear();
-        self.num2.clear();
-    }
-
-    fn next_state(&self, c: char) -> Option<ParsingState> {
-        match (&self.state, c) {
-            (ParsingState::Instruction, '(') => Some(ParsingState::FirstNum),
-            (ParsingState::FirstNum, ',') => Some(ParsingState::SecondNum),
-            (ParsingState::SecondNum, ')') => Some(ParsingState::Final),
-            _ => None,
+    pub fn pop(&mut self) -> Option<String> {
+        let c = self.chars.get(self.cursor..self.cursor + 1);
+        if let Some(_) = c {
+            self.cursor += 1;
         }
+
+        c.map(|v| v.to_string())
+    }
+
+    pub fn at_end(&self) -> bool {
+        self.chars.len() == self.cursor
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::part1;
+    use super::{part1, part2};
 
     #[test]
     fn test_success() {
         let preview_input = "mul(10,5)";
         assert_eq!("50", part1(preview_input));
+    }
+
+    #[test]
+    fn test_unclosed_bracket() {
+        let preview_input = "mul(257,900@{select(66,790)";
+        assert_eq!("0", part1(preview_input));
     }
 
     #[test]
@@ -125,5 +170,11 @@ mod test {
     fn test_preview_part1() {
         let preview_input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         assert_eq!("161", part1(preview_input));
+    }
+
+    #[test]
+    fn test_preview_part2() {
+        let preview_input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!("48", part2(preview_input));
     }
 }
