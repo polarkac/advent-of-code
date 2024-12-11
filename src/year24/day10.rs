80d8b9cb5db8054e985591015b40f8ca90495754
Lukas Pohlreich <lukas@pohlreichlukas.eu>
2024-12-11 17:15:36 +0100

    2024-10 First solution

---
+++ src/year24/day10.rs
@@ -0,0 +1,175 @@
+use std::{collections::{HashMap, HashSet}, fmt::Display};
+
+use crate::include_input;
+
+pub fn input() -> &'static str {
+    include_input!(2024 / 10)
+}
+
+pub fn part1(input: &str) -> String {
+    let map = HeightMap::from_input(input);
+
+    map.count_reachable_tops().to_string()
+}
+
+struct HeightMap {
+    map: Vec<Vec<u32>>,
+    trail_heads: Vec<(usize, usize)>,
+}
+
+impl HeightMap {
+    fn from_input(input: &str) -> Self {
+        let mut trail_heads = Vec::new();
+        let map = input.lines().map(|line| line.trim()).enumerate().fold(
+            Vec::new(),
+            |mut acc, (y, line)| {
+                let new_line = line.chars().enumerate().fold(
+                    Vec::new(),
+                    |mut acc, (x, c)| {
+                        let height = c.to_digit(10).unwrap_or(100);
+                        if height == 0 {
+                            trail_heads.push((y, x));
+                        }
+                        acc.push(height);
+
+                        acc
+                    },
+                );
+
+                acc.push(new_line);
+
+                acc
+            },
+        );
+
+        Self { map, trail_heads }
+    }
+
+    fn count_reachable_tops(&self) -> usize {
+        let mut visited_tops = HashMap::new();
+        for head in &self.trail_heads {
+            visited_tops.insert(*head, self.try_reach_top(*head));
+        }
+
+        visited_tops.iter().map(|(_, tops)| tops.len()).sum()
+    }
+
+    fn try_reach_top(&self, head: (usize, usize)) -> HashSet<(usize, usize)> {
+        let mut set = HashSet::new();
+        let current_height = self.map[head.0][head.1];
+        if current_height == 9 {
+            set.insert((head.0, head.1));
+
+            return set;
+        }
+
+        let up_pos = Direction::Up.next_position(head, self.size());
+        let down_pos = Direction::Down.next_position(head, self.size());
+        let left_pos = Direction::Left.next_position(head, self.size());
+        let right_pos = Direction::Right.next_position(head, self.size());
+
+        if let Some((y, x)) = up_pos {
+            let height = self.map[y][x].checked_sub(1);
+            if let Some(height) = height {
+                if current_height == height {
+                    set.extend(self.try_reach_top((y, x)));
+                }
+            }
+        }
+        if let Some((y, x)) = down_pos {
+            let height = self.map[y][x].checked_sub(1);
+            if let Some(height) = height {
+                if current_height == height {
+                    set.extend(self.try_reach_top((y, x)));
+                }
+            }
+        }
+        if let Some((y, x)) = left_pos {
+            let height = self.map[y][x].checked_sub(1);
+            if let Some(height) = height {
+                if current_height == height {
+                    set.extend(self.try_reach_top((y, x)));
+                }
+            }
+        }
+        if let Some((y, x)) = right_pos {
+            let height = self.map[y][x].checked_sub(1);
+            if let Some(height) = height {
+                if current_height == height {
+                    set.extend(self.try_reach_top((y, x)));
+                }
+            }
+        }
+
+        set
+    }
+
+    fn size(&self) -> (usize, usize) {
+        (self.map.len(), self.map[0].len())
+    }
+}
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn next_position(
+        &self,
+        position: (usize, usize),
+        size: (usize, usize),
+    ) -> Option<(usize, usize)> {
+        let next_position = match self {
+            Self::Up => (position.0.checked_sub(1), Some(position.1)),
+            Self::Down => (position.0.checked_add(1), Some(position.1)),
+            Self::Left => (Some(position.0), position.1.checked_sub(1)),
+            Self::Right => (Some(position.0), position.1.checked_add(1)),
+        };
+
+        match next_position {
+            (Some(y), Some(x)) => {
+                if y >= size.0 || x >= size.1 {
+                    return None;
+                }
+                Some((y, x))
+            }
+            _ => None,
+        }
+    }
+}
+
+impl Display for HeightMap {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut map_str = String::new();
+        for line in &self.map {
+            for c in line {
+                map_str.push_str(&c.to_string());
+            }
+            map_str.push('\n');
+        }
+
+        write!(f, "{map_str}").and_then(|_| write!(f, "{:?}", self.trail_heads))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::year24::day10::part1;
+
+    const INPUT_PREVIEW: &str = "89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732";
+
+    #[test]
+    fn test_preview_part1() {
+        assert_eq!("36", part1(INPUT_PREVIEW));
+    }
+}
--- src/year24/mod.rs
+++ src/year24/mod.rs
@@ -9,51 +9,44 @@ mod day06;
 mod day07;
 mod day08;
 mod day09;
+mod day10;
 
 pub fn build_aoc(args: &Args) -> Result<Aoc<'_>, Error> {
     match args.day.as_u8() {
-        1 => Ok(
-            Aoc::new(args, Box::new(day01::input))
-                .part(Box::new(day01::part1))
-                .part(Box::new(day01::part2)),
-        ),
-        2 => Ok(
-            Aoc::new(args, Box::new(day02::input))
-                .part(Box::new(day02::part1))
-                .part(Box::new(day02::part2))
-        ),
-        3 => Ok(
-            Aoc::new(args, Box::new(day03::input))
-                .part(Box::new(day03::part1))
-                .part(Box::new(day03::part2))
-        ),
-        4 => Ok(
-            Aoc::new(args, Box::new(day04::input))
-                .part(Box::new(day04::part1))
-                .part(Box::new(day04::part2))
-        ),
-        5 => Ok(
-            Aoc::new(args, Box::new(day05::input))
-                .part(Box::new(day05::part1))
-                .part(Box::new(day05::part2))
-        ),
-        6 => Ok(
-            Aoc::new(args, Box::new(day06::input))
-                .part(Box::new(day06::part1))
-        ),
-        7 => Ok(
-            Aoc::new(args, Box::new(day07::input))
-                .part(Box::new(day07::part1))
-                .part(Box::new(day07::part2))
-        ),
-        8 => Ok(
-            Aoc::new(args, Box::new(day08::input))
-                .part(Box::new(day08::part1))
-        ),
-        9 => Ok(
-            Aoc::new(args, Box::new(day09::input))
-                .part(Box::new(day09::part1))
-        ),
+        1 => Ok(Aoc::new(args, Box::new(day01::input))
+            .part(Box::new(day01::part1))
+            .part(Box::new(day01::part2))),
+        2 => Ok(Aoc::new(args, Box::new(day02::input))
+            .part(Box::new(day02::part1))
+            .part(Box::new(day02::part2))),
+        3 => Ok(Aoc::new(args, Box::new(day03::input))
+            .part(Box::new(day03::part1))
+            .part(Box::new(day03::part2))),
+        4 => Ok(Aoc::new(args, Box::new(day04::input))
+            .part(Box::new(day04::part1))
+            .part(Box::new(day04::part2))),
+        5 => Ok(Aoc::new(args, Box::new(day05::input))
+            .part(Box::new(day05::part1))
+            .part(Box::new(day05::part2))),
+        6 => {
+            Ok(Aoc::new(args, Box::new(day06::input))
+                .part(Box::new(day06::part1)))
+        }
+        7 => Ok(Aoc::new(args, Box::new(day07::input))
+            .part(Box::new(day07::part1))
+            .part(Box::new(day07::part2))),
+        8 => {
+            Ok(Aoc::new(args, Box::new(day08::input))
+                .part(Box::new(day08::part1)))
+        }
+        9 => {
+            Ok(Aoc::new(args, Box::new(day09::input))
+                .part(Box::new(day09::part1)))
+        }
+        10 => {
+            Ok(Aoc::new(args, Box::new(day10::input))
+                .part(Box::new(day10::part1)))
+        }
         _ => Err(Error::InvalidDay),
     }
 }
